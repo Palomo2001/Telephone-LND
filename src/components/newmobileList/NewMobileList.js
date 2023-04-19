8b4f32b029fbe08d381cdda26bb4a98e9cb1376a
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2023-04-19 15:38:37 +0100

    NewMobileList

---
+++ src/components/newmobileList/NewMobileList.js
@@ -0,0 +1,62 @@
+import NewTelephoneService from "../../services/NewTelephone.service";
+import { useState, useEffect } from "react";
+import "./NewMobileList.css";
+
+function NewMobileList() {
+    const [Newmobile, setNewTelephone] = useState([]);
+    const [query, setQuery] = useState("");
+  
+  
+    const getNewTelephone = () => {
+      const allNewMobile = NewTelephoneService.getNewTelephone();
+      setNewTelephone(allNewMobile);
+    }
+  
+    const showNewMobile = () => {
+      return (
+        Newmobile.filter(c => 
+          c.brand.toLowerCase().includes(query.toLowerCase()) || 
+          c.model.toLowerCase().includes(query.toLowerCase()) ||
+          c.price.toLowerCase().includes(query.toLowerCase()))
+          .map(c => {
+          return (
+            <div className="mobile-item">
+              <div className="mobile-img">
+                <img src={`/assets/img/${c.img}`} alt="mobile" />
+                <img src={`/assets/img/${c.img}`} alt="mobile" />
+                <img src={`/assets/img/${c.img}`} alt="mobile" />
+              </div>
+              <div className="text-mobile">
+              <p>{c.brand}</p>
+              <p>{c.model}</p>
+              <p>{c.price}</p>
+              </div>
+            </div>
+          );
+        })
+      );
+    }
+    const handleChange = (event) => {
+      const searchValue = event.target.value;
+      setQuery(searchValue);
+    }
+  
+  
+    useEffect(
+      () => {
+        getNewTelephone();
+      }, []);
+  
+    return (
+      <>
+      <input type="search" onChange={handleChange}/>
+        <div className="main-container">
+          <div className="mobile-container">
+            {showNewMobile()}
+          </div>
+        </div> 
+      </>
+    );
+  }
+  
+  export default NewMobileList;
--- src/services/NewTelephone.services.js
+++ src/services/NewTelephone.services.js
@@ -1,54 +1,49 @@
 const mobile = [
     {
-        price: "120.00€",
-        model: "Galaxy A23",
+        price: "1799,9€",
+        model: "Samsung Galaxy Z Fold4",
         brand: "Samsung",
-        img: "GalaxyA23.jpg"
+        img: "samsungGalaxyZFold.jpg"
     }, {
-        price: "210.94€",
-        model: "A95",
-        brand: "Blacview",
-        img: "A95.jpg"
+        price: "210,94",
+        model: "Oppo Find X5",
+        brand: "OPPO",
+        img: "oppofindx5pro.jpg"
     }, {
-        price: "729.00€",
-        model: "Apple iPhone 12",
-        brand: "Iphone",
-        img: "iphone12.jpg"
-    }, {
-        price: "24.00€",
-        model: "Redmi 9C",
-        brand: "Xiaomi",
-        img: "redmi9c.jpg"
-    }, {
-        price: "285.19€",
-        model: "Nova 9",
-        brand: "Huawei",
-        img: "nova9.jpg"
-    }, {
-        price: "249.00€",
-        model: "Galaxy M33",
-        brand: "Samsung",
-        img: "galaxym33.jpg"
-    }, {
-        price: "259.28€",
-        model: "BV7100",
-        brand: "Blackview",
-        img: "BV7100.jpg"
-    }, {
-        price: "729.00€",
-        model: "Apple iPhone 14 Pro Max2",
+        price: "1331,4€",
+        model: "Apple iPhone 14 Pro Max",
         brand: "Iphone",
         img: "AppleIphone14pro.jpg"
     }, {
-        price: "254.21€",
-        model: "Redmi Note 10 Pro 8",
+        price: "899,9€",
+        model: "Xiaomi 12 Pro",
         brand: "Xiaomi",
-        img: "redminote10.jpg"
+        img: "Xiaomi12PRO.jpg"
     }, {
-        price: "150.00€",
-        model: "Magic5 Lite",
-        brand: "Huawei",
-        img: "magiclite.jpg"
+        price: "699,9€",
+        model: "Realme GT 2 Pro",
+        brand: "Realme",
+        img: "realmegt2pro.jpg"
+    }, {
+        price: "249,00€",
+        model: "Vivo Y22s 6",
+        brand: "Vivo",
+        img: "vivoY22s6.jpg"
+    }, {
+        price: "129,9€",
+        model: "Motorola Moto e22",
+        brand: "Motorola", 
+        img: "MotorolaMotoe22.jpg"
+    }, {
+        price: "449,9€",
+        model: "Honor 70",
+        brand: "Honor",        
+        img: "AppleIphone14pro.jpg"                 
+    }, {
+        price: "419,9€",
+        model: "Xiaomi 12 Lite",
+        brand: "Xiaomi2",
+        img: "redminote10.jpg"
     }
 ];
 
