a1698535579632f09b2f30a6271aa5aefed6b125
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2023-04-19 15:24:51 +0100

    second commit

---
+++ src/services/NewTelephone.services.js
@@ -0,0 +1,61 @@
+const mobile = [
+    {
+        price: "120.00€",
+        model: "Galaxy A23",
+        brand: "Samsung",
+        img: "GalaxyA23.jpg"
+    }, {
+        price: "210.94€",
+        model: "A95",
+        brand: "Blacview",
+        img: "A95.jpg"
+    }, {
+        price: "729.00€",
+        model: "Apple iPhone 12",
+        brand: "Iphone",
+        img: "iphone12.jpg"
+    }, {
+        price: "24.00€",
+        model: "Redmi 9C",
+        brand: "Xiaomi",
+        img: "redmi9c.jpg"
+    }, {
+        price: "285.19€",
+        model: "Nova 9",
+        brand: "Huawei",
+        img: "nova9.jpg"
+    }, {
+        price: "249.00€",
+        model: "Galaxy M33",
+        brand: "Samsung",
+        img: "galaxym33.jpg"
+    }, {
+        price: "259.28€",
+        model: "BV7100",
+        brand: "Blackview",
+        img: "BV7100.jpg"
+    }, {
+        price: "729.00€",
+        model: "Apple iPhone 14 Pro Max2",
+        brand: "Iphone",
+        img: "AppleIphone14pro.jpg"
+    }, {
+        price: "254.21€",
+        model: "Redmi Note 10 Pro 8",
+        brand: "Xiaomi",
+        img: "redminote10.jpg"
+    }, {
+        price: "150.00€",
+        model: "Magic5 Lite",
+        brand: "Huawei",
+        img: "magiclite.jpg"
+    }
+];
+
+const getTelephone = () => {
+    return mobile;
+}
+
+export default {
+    getTelephone
+};
