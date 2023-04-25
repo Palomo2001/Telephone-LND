67dd892e99ef0e3f3cd7b8b80ebf912e3ae92c5e
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2023-04-25 16:12:39 +0100

    footerFinish

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,22 @@
 import "./Footer.css"
 
-function Footer(){
-    return(
-        <div id="container">
-            <div className="text">
-            <p>Contáctanos al 444-444-444</p> 
-            <p>SoporteTecnico@PalomoMobile.com</p>
-            <p>Copyright 2023© Todos los derechos reservados</p>
-            <p>Para ver el aviso de privacidad, accede a PalomoMobile.com/aviso</p>
-            </div>
-            <div className="icon">
-            <ion-icon name="logo-twitter"></ion-icon>
-            <ion-icon name="logo-facebook"></ion-icon>
-            <ion-icon name="logo-youtube"></ion-icon>
-            <ion-icon name="logo-instagram"></ion-icon>
-            </div>
-        </div>
-    );
+function Footer() {
+  return (
+    <div id="container">
+      <div className="text">
+        <p>Contáctanos al 444-444-444</p>
+        <p>SoporteTecnico@PalomoMobile.com</p>
+        <p>Copyright 2023© Todos los derechos reservados</p>
+        <p>Para ver el aviso de privacidad, accede a PalomoMobile.com/aviso</p>
+      </div>
+      <div className="icon">
+        <ion-icon name="logo-twitter"></ion-icon>
+        <ion-icon name="logo-facebook"></ion-icon>
+        <ion-icon name="logo-youtube"></ion-icon>
+        <ion-icon name="logo-instagram"></ion-icon>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
