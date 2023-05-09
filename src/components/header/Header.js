db75e292ea4a4715fbc7e37fbe4e6799664aabaf
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2023-05-09 16:57:43 +0100

    IntroducirBotonNohecho

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,10 +4,10 @@ function Header(){
     return(
         <ul>
             <li><img src="/assets/img/logo.png" alt="logo"/></li>
-            <li><a href="/home">Moviles</a></li>
-            <li><a href="/computer">Portatil</a></li>
-            <li><a href="/Tablet">Tablet</a></li>
-            <li><a href="/Extras">Extras</a></li>
+            <li className="menu"><a href="/home">Móviles</a></li>
+            <li className="menu"><a href="/computer">Portatil</a></li>
+            <li className="menu"><a href="/Tablet">Tablet</a></li>
+            <li className="menu"><a href="/Extras">Extras</a></li>
         </ul>
     );
 }
