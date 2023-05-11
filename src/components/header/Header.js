911439d39232cfd366d79f77bbff83948881bd88
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2023-05-11 09:27:57 +0100

    Hamburgesa

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,36 @@
 import "./Header.css"
+import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
 
 function Header(){
+    const [menuVisible,setMenuVisible]=useState(false);
+
+    const toggleCollapsedMenu = () => {
+        setMenuVisible(!menuVisible);
+    }
     return(
+        <>
         <ul>
             <li><img src="/assets/img/logo.png" alt="logo"/></li>
-            <li className="menu"><a href="/home">Móviles</a></li>
-            <li className="menu"><a href="/computer">Portatil</a></li>
-            <li className="menu"><a href="/Tablet">Tablet</a></li>
-            <li className="menu"><a href="/Extras">Extras</a></li>
+            <li className="menu-option"><a href="/home">Móviles</a></li>
+            <li className="menu-option"><a href="/computer">Portatil</a></li>
+            <li className="menu-option"><a href="/Tablet">Tablet</a></li>
+            <li className="menu-option"><a href="/Extras">Extras</a></li>
+            <li className="menu-option" onClick={toggleCollapsedMenu}><FaBars /></li>
         </ul>
+        {menuVisible ? 
+            <div className="collapsed-menu">
+                <ul>
+                    <li><a href="/home">Móviles</a></li>
+                    <li><a href="/computer">Portatil</a></li>
+                    <li><a href="/Tablet">Tablet</a></li>
+                    <li><a href="/Extras">Extras</a></li>
+                </ul>
+            </div>
+            :
+            <></>
+        }
+        </>
     );
 }
 
